be32edcc293741559bb576c3a7c536112b825236
Steve Levy <stlevy002@sbcglobal.net>
2021-06-20 16:27:15 -0400

    Add It

---
+++ client/user-scripts.js
@@ -0,0 +1,61 @@
+/* *************************
+*** USER SIGNUP ***
+************************** */
+function userSignUp() {
+    //  console.log('userSignUp Function Called')
+
+    let userEmail = document.getElementById("emailSignup").value;
+    let userPass = document.getElementById("pwdSignup").value;
+
+    let newUserData = {
+        user: {
+            email: userEmail,
+            password: userPass
+        }
+    };
+
+    console.log(`newUserData --> ${newUserData.user.email} ${newUserData.user.password}`);
+
+    fetch(`http://localhost:3000/user/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUserData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let token = data.sessionToken;
+            localStorage.setItem('SessionToken', token);
+            tokenChecker();
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    };   
+    
+    /* *************************
+    *** USER LOGIN ***
+    ************************** */
+    function userLogin() {
+    //  console.log('userLogin Function Called')
+    
+    }
+    
+    
+    /* *************************
+    *** USER LOGOUT ***
+    ************************** */
+    function userLogout() {
+     console.log('userLogout Function Called')
+    }
+    
+    
+    /* *************************
+     *** TOKEN CHECKER FUNCTION ***
+    ************************** */
+    function tokenChecker() {
+     console.log('tokenChecker Function Called')
+    }
+    tokenChecker()
--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const Express = require("express");
 const app = Express();
 const dbConnection = require("./db");
+
+app.use(require('./middleware/headers'));
 // app.use('/test', (req, res) => {res.send('This is a message from the test endpoint on the server!')})
 const controllers = require("./controllers");
 
